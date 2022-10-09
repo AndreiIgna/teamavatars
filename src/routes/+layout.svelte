a3092d38d150281a31a5f8a9c669d8d562cb10d9
Andrei <andrei@laye.red>
2022-10-09 18:06:21 +0300

    Added - more avatars, Svelte Kit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+<script>
+import '../app.css';
+</script>
+
+<div class="app">
+
+	<div class="fixed-top">
+		<nav class="navbar navbar-fixed navbar-expand-lg px-3">
+		
+			<a class="navbar-brand" href="/">Team Avatars</a>
+
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link" href="#styles">Styles</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#how-it-works">How it works</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#pricing">Pricing</a>
+					</li>
+				</ul>
+			</div>
+
+			<ul class="navbar-nav mb-2 mb-lg-0">
+				<li class="nav-item ms-6">
+					<a class="nav-link btn btn-primary text-light" href="#pricing">Start a project</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+
+	<div style="min-height: 80vh">
+		<slot></slot>
+	</div>
+
+	<footer style="background-color: #e2e8f0">
+		<div class="container p-2">
+			<div class="row">
+				<div class="col my-2">
+					Made by <a href="https://laye.red" target="_blank">Layered</a> &middot; <a href="https://www.instagram.com/yogatella/" target="_blank">@yogatella</a>
+				</div>
+				<div class="col text-end my-1">
+					<a href="mailto:denisa@yogatella.com?subject=Custom team avatars" target="_blank">Contact us</a>
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,357 @@
+
+<svelte:head>
+	<title>Team avatars - Personalized avatar illustrations</title>
+	<meta name="description" content="Create personalized avatars for your team">
+
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://teamavatars.com">
+	<meta property="og:title" content="Team avatars - Personalized avatar illustrations">
+	<meta property="og:description" content="Create personalized avatars for your team">
+	<meta property="og:image" content="https://teamavatars.com/link-preview.jpg">
+
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:url" content="https://teamavatars.com">
+	<meta name="twitter:title" content="Team avatars - Personalized avatar illustrations">
+	<meta name="twitter:description" content="Create personalized avatars for your team">
+	<meta name="twitter:image" content="https://teamavatars.com/link-preview.jpg">
+	<meta name="twitter:site" content="@LayeredHQ" />
+</svelte:head>
+
+
+<header class="container-fluid my-6 py-5 text-center px-3">
+	<h1 class="mb-4"><span class="text-pink">We create</span> ✍️ <span class="text-blue">the best avatars</span> <img src="/avatars/colorful-1.jpg" class="avatar" alt="Avatar 1"> <img src="/avatars/quirky-5.jpg" class="avatar" alt="Avatar 2"> <span class="text-lightblue">on the internet</span> <img src="/avatars/colorful-12.jpg" class="avatar" alt="Avatar 3"> <img src="/avatars/watercolor-7.jpg" class="avatar" alt="Avatar 4"></h1>
+
+	<h3 class="text-blue mb-5">and we do it virtually. Send us some pics of your team!</h3>
+
+	<a href="#pricing" class="btn btn-lg btn-primary">Book a project</a>
+</header>
+
+<section class="section-clients pt-4 pb-4">
+	<div class="container text-center">
+		<h5 class="mb-4">Companies ❤️ us</h5>
+
+		<div class="row row-cols-3 row-cols-lg-6 gx-5 align-items-center justify-content-center">
+			<div class="col my-1">
+				<img src="https://assets.website-files.com/5fd9ec20a182fdcd717d73c6/609c149075fd9aeb72cf78b7_Town_Web_Trademark-p-500.png" class="img-fluid" alt="Client - TownWeb" />
+			</div>
+			<div class="col my-1">
+				<img src="https://trailrunningacademy.com/wp-content/uploads/sites/6/2017/10/cropped-logo-e1616063123138.png" height="60" alt="Client - TrailRunning Academy" />
+			</div>
+			<div class="col my-1">
+				<img src="https://cdn.shopify.com/s/files/1/3098/7218/files/tako-logo_140x@2x.png?v=1613556592" class="img-fluid" alt="Client - Tako Agency" />
+			</div>
+			<div class="col my-1">
+				<img src="https://assets.website-files.com/60c0bf119dbe7a93934b9782/6211ee344739a08c53957074_HeyGov%203.0.svg" height="60" alt="Client - HeyGov" />
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="styles" class="section-styles container-fluid py-6">
+
+	<h2 class="text-center mb-4">Avatar styles</h2>
+
+	<div class="row">
+		<div class="col-lg-6 my-3">
+			<div class="box-blue p-3">
+				<h4 class="box-title mb-2 text-center">Colorful clean</h4>
+
+				<div class="row">
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-1.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 1">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-2.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 2">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-3.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 3">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-4.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 4">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-5.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 5">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-6.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 6">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-7.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 7">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-8.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 8">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-9.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 9">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-10.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 10">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-11.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 11">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/colorful-12.jpg" class="img-fluid rounded-circle" alt="Colorful Avatar 12">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-6 my-3">
+			<div class="box-pink p-3">
+				<h4 class="box-title mb-2 text-center">Minimalist / Line drawing</h4>
+
+				<div class="row">
+					<div class="col-3 my-2">
+						<img src="/avatars/min-1.jpg" class="img-fluid rounded-circle" alt="Minimalist Avatar 1">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/min-2.jpg" class="img-fluid rounded-circle" alt="Minimalist Avatar 2">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/min-3.jpg" class="img-fluid rounded-circle" alt="Minimalist Avatar 3">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/min-4.jpg" class="img-fluid rounded-circle" alt="Minimalist Avatar 4">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/min-5.jpg" class="img-fluid rounded-circle" alt="Minimalist Avatar 5">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/min-6.jpg" class="img-fluid rounded-circle" alt="Minimalist Avatar 6">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/min-7.jpg" class="img-fluid rounded-circle" alt="Minimalist Avatar 7">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/min-8.jpg" class="img-fluid rounded-circle" alt="Minimalist Avatar 8">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/min-9.jpg" class="img-fluid rounded-circle" alt="Minimalist Avatar 9">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-6 my-3">
+			<div class="box-green p-3">
+				<h4 class="box-title mb-2 text-center">Watercolor</h4>
+
+				<div class="row">
+					<div class="col-3 my-2">
+						<img src="/avatars/watercolor-1.jpg" class="img-fluid rounded-circle" alt="Avatar watercolor 1">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/watercolor-2.jpg" class="img-fluid rounded-circle" alt="Avatar watercolor 2">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/watercolor-3.jpg" class="img-fluid rounded-circle" alt="Avatar watercolor 3">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/watercolor-4.jpg" class="img-fluid rounded-circle" alt="Avatar watercolor 4">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/watercolor-5.jpg" class="img-fluid rounded-circle" alt="Avatar watercolor 5">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/watercolor-6.jpg" class="img-fluid rounded-circle" alt="Avatar watercolor 6">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/watercolor-7.jpg" class="img-fluid rounded-circle" alt="Avatar watercolor 7">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-6 my-3">
+			<div class="box-blue p-3">
+				<h4 class="box-title mb-2 text-center">Colorful quirky</h4>
+
+				<div class="row">
+					<div class="col-3 my-2">
+						<img src="/avatars/quirky-1.jpg" class="img-fluid rounded-circle" alt="quirky Avatar 1">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/quirky-2.jpg" class="img-fluid rounded-circle" alt="quirky Avatar 2">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/quirky-3.jpg" class="img-fluid rounded-circle" alt="quirky Avatar 3">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/quirky-4.jpg" class="img-fluid rounded-circle" alt="quirky Avatar 4">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/quirky-5.jpg" class="img-fluid rounded-circle" alt="quirky Avatar 5">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/quirky-6.jpg" class="img-fluid rounded-circle" alt="quirky Avatar 6">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/quirky-7.jpg" class="img-fluid rounded-circle" alt="quirky Avatar 7">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/quirky-8.jpg" class="img-fluid rounded-circle" alt="quirky Avatar 8">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-6 my-3">
+			<div class="box-pink p-3">
+				<h4 class="box-title mb-2 text-center">Realistic</h4>
+
+				<div class="row">
+					<div class="col-3 my-2">
+						<img src="/avatars/realistic-1.jpg" class="img-fluid rounded-circle" alt="realistic Avatar 1">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/realistic-2.jpg" class="img-fluid rounded-circle" alt="realistic Avatar 2">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/realistic-3.jpg" class="img-fluid rounded-circle" alt="realistic Avatar 3">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/realistic-4.jpg" class="img-fluid rounded-circle" alt="realistic Avatar 4">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/realistic-5.jpg" class="img-fluid rounded-circle" alt="realistic Avatar 5">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/watercolor-7.jpg" class="img-fluid rounded-circle" alt="realistic Avatar 7">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-6 my-3">
+			<div class="box-green p-3">
+				<h4 class="box-title mb-2 text-center">Anime / Manga</h4>
+
+				<div class="row">
+					<div class="col-3 my-2">
+						<img src="/avatars/anime-1.jpg" class="img-fluid rounded-circle" alt="Anime Avatar 1">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/anime-2.jpg" class="img-fluid rounded-circle" alt="Anime Avatar 2">
+					</div>
+					<div class="col-3 my-2">
+						<img src="/avatars/anime-3.jpg" class="img-fluid rounded-circle" alt="Anime Avatar 3">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="how-it-works" class="section-styles container py-6">
+
+	<h2 class="text-center mb-4">The process</h2>
+
+	<div class="row">
+		<div class="col-lg-4 my-3">
+			<img src="/img-book.jpg" alt="Team Avatars - book" class="img-fluid rounded mb-3">
+			<h3>Book.</h3>
+			<p class="lead">Choose the style and tell us how many people are in your team.</p>
+		</div>
+		<div class="col-lg-4 my-3">
+			<img src="/img-chat.jpg" alt="Team Avatars - call" class="img-fluid rounded mb-3">
+			<h3>Chat.</h3>
+			<p class="lead">We'll  make some sketches and show you the progress.</p>
+		</div>
+		<div class="col-lg-4 my-3">
+			<img src="/img-send.jpg" alt="Team Avatars - send" class="img-fluid rounded mb-3">
+			<h3>Receive.</h3>
+			<p class="lead">You'll receive the final avatars in high resolution, ready to use.</p>
+		</div>
+	</div>
+</section>
+
+<section id="pricing" class="section-pricing container-fluid">
+	<div class="row gx-0">
+		<div class="col-lg-6">
+			<div class="pricing-1 text-center py-5">
+				<h4 class="mb-4">avatars starting from $89</h4>
+				<h6 class="mb-5">choose every detail and build a project that is right for your budget.</h6>
+
+				<p class="my-5">
+					<img src="/avatars/colorful-1.jpg" width="200" height="200" alt="Avatar colorful" class="rounded-circle" />
+				</p>
+
+				<a href="https://buy.stripe.com/4gwcOC61D0iCeYMbII" target="team-avatars-buy" class="btn btn-lg btn-primary">Book now</a>
+			</div>
+		</div>
+		<div class="col-lg-6">
+			<div class="pricing-2 text-center py-5">
+				<h4 class="mb-4">packs of 10 start at $500</h4>
+				<h6 class="mb-5">expertly curated packs tailored to your exact needs.</h6>
+
+				<p class="my-5">
+					<img src="/avatars/pack.jpg" width="200" height="200" alt="Avatar colorful" class="rounded-circle" />
+				</p>
+
+				<a href="https://buy.stripe.com/4gwcOC61D0iCeYMbII" target="team-avatars-buy" class="btn btn-lg btn-primary">Book now</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style>
+.box-blue {
+	background-color: rgba(147, 162, 248, .15);
+	border-radius: 1rem;
+}
+.box-blue .box-title {
+	color: #93a2f8;
+	font-family: 'Bebas Neue', cursive;
+}
+
+.box-green {
+	background-color: rgba(68, 154, 85, .15);
+	border-radius: 1rem;
+}
+.box-green .box-title {
+	color: #449a55;
+	
+}
+
+.box-pink {
+	background-color: rgba(243, 184, 246, .15);
+	border-radius: 1rem;
+}
+.box-pink .box-title {
+	color: #f3b8f6;
+	
+}
+
+header h1 {
+	font-family: 'Bebas Neue', cursive;
+	display: inline-block;
+	font-size: 9.5vw;
+	line-height: 1.05;
+	font-weight: 400;
+}
+
+header h3 {
+	font-size: 2rem;
+}
+
+header img {
+	display: inline-block;
+	width: 9.5vw;
+	height: 9.5vw;
+	border-radius: 9.5vw;
+}
+
+.section-clients {
+	background-color: #fdf0fe;
+	color: #666;
+}
+
+.section-clients img {
+	filter: grayscale(100%);
+}
+
+.pricing-1 {
+	background-color: #c2e0e7;
+}
+
+.pricing-2 {
+	background-color: #e3d896;
+}
+
+</style>
